b77fa41a20801b1e3c8a1b48f7d52163163aface
troosan <antoine.de.troostembergh@gmail.com>
2018-05-24 00:10:08 +0200

    fix typos

--- tests/Common/Tests/Microsoft/PasswordEncoderTest.php
+++ tests/Common/Tests/Microsoft/PasswordEncoderTest.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * This file is part of PHPWord - A pure PHP library for reading and writing
- * word processing documents.
+ * This file is part of PHPOffice Common
  *
- * PHPWord is free software distributed under the terms of the GNU Lesser
+ * PHPOffice Common is free software distributed under the terms of the GNU Lesser
  * General Public License version 3 as published by the Free Software Foundation.
  *
  * For the full copyright and license information, please read the LICENSE
  * file that was distributed with this source code. For the full list of
- * contributors, visit https://github.com/PHPOffice/PHPWord/contributors.
+ * contributors, visit https://github.com/PHPOffice/Common/contributors.
  *
- * @see         https://github.com/PHPOffice/PHPWord
- * @copyright   2010-2017 PHPWord contributors
+ * @link        https://github.com/PHPOffice/Common
+ * @copyright   2009-2016 PHPOffice Common contributors
  * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  */
 
@@ -59,7 +58,7 @@ class PasswordEncoderTest extends \PHPUnit_Framework_TestCase
     /**
      * Test that the encoder falls back on SHA-1 if a non supported algorithm is given
      */
-    public function testDafaultsToSha1IfUnsupportedAlgorithm()
+    public function testDefaultsToSha1IfUnsupportedAlgorithm()
     {
         //given
         $password = 'test';
